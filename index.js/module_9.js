78dea3e167df16a2c110fa8052eaee6f9f636474
nataliiasirenko <sirenko.nataliia@hotmail.com>
2023-01-03 15:02:24 +0000

    update

---
+++ index.js/module_9.js
@@ -0,0 +1,64 @@
+// const date = new Date();
+
+// console.log(date);
+// console.log(date.toString());
+// console.log(new Date(0));
+// console.log(new Date(15000));
+
+// const date = new Date();
+// console.log(date.getTime());
+
+// const teamMeetingDate = new Date("March 16, 2030");
+// console.log(teamMeetingDate);
+
+// const date = new Date();
+// console.log("Date: ", date);
+
+// // Повертає день місяця від 1 до 31
+// console.log("getDate(): ", date.getDate());
+// console.log("getDay(): ", date.getDay());
+// console.log("getMonth(): ", date.getMonth());
+// console.log("getFullYear(): ", date.getFullYear());
+// console.log("getHours(): ", date.getHours());
+// console.log("getMinutes(): ", date.getMinutes());
+// console.log("getSeconds(): ", date.getSeconds());
+// console.log("getMilliseconds(): ", date.getMilliseconds());
+
+// console.log("getUTCDate(): ", date.getUTCDate());
+
+// const date = new Date("March 16, 2030 14:25:00");
+
+// date.setMinutes(50);
+// // "Sat Mar 16 2030 14:50:00 GMT+0200"
+
+// date.setFullYear(2040, 4, 8);
+// console.log(date);
+// // "Tue May 08 2040 14:50:00 GMT+0300"
+
+// const date = new Date("March 16, 2030 14:25:00");
+
+// date.toString();
+// "Sat Mar 16 2030 14:25:00 GMT+0200 (Eastern European Standard Time)"
+
+// date.toTimeString();
+// // "14:25:00 GMT+0200 (Eastern European Standard Time)"
+
+// date.toLocaleTimeString();
+// // "2:25:00 PM"
+
+// date.toUTCString();
+// // "Sat, 16 Mar 2030 12:25:00 GMT"
+
+// date.toDateString();
+// // "Sat Mar 16 2030"
+
+// date.toISOString();
+// // "2030-03-16T12:25:00.000Z"
+
+// date.toLocaleString();
+// // "3/16/2030, 2:25:00 PM"
+
+// date.getTime();
+// // 1899894300000
+
+// console.log(date.toDateString());
